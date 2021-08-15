372c01cfddea78635b50190bc8e1596a95cac544
jul1nho <jcbjr28@gmail.com>
2021-08-15 18:09:24 -0300

    adicionado os menus

---
+++ index.php
@@ -0,0 +1,66 @@
+<!doctype html>
+<hmtl lang="pt-br">
+    <head>
+     <!--Required meta tags-->
+      <meta charset="utf-8">
+      <meta name="viweport" conte="width=device-width, inital-scale=1, shrink-to-fit=n">
+
+      <!-- Bootstrap CSS -->
+      <link rel="stylesheet" href="https://strackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css">
+      <link rel="stylesheet" href="css/style.css">
+
+
+      <title>La de casa</title>
+    </head>
+ <body>
+<div id="main-menu">
+   
+   <nav class="navbar navbar-expand-lg nav-new">
+  <a class="navbar-brand" href="#">Navbar</a>
+  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+    <span class="navbar-toggler-icon"></span>
+  </button>
+
+  <div class="collapse navbar-collapse" id="navbarSupportedContent">
+    <ul class="navbar-nav mr-auto">
+      <li class="nav-item active">
+        <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="#">Link</a>
+      </li>
+      <li class="nav-item dropdown">
+        <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+          Dropdown
+        </a>
+        <div class="dropdown-menu" aria-labelledby="navbarDropdown">
+          <a class="dropdown-item" href="#">Action</a>
+          <a class="dropdown-item" href="#">Another action</a>
+          <div class="dropdown-divider"></div>
+          <a class="dropdown-item" href="#">Something else here</a>
+        </div>
+      </li>
+      
+     </ul>
+    
+       </div>
+     </nav>
+</div>
+    <div class="row">
+        <div class="col-sm-4">
+
+        </div>
+        <div class="col-sm-8">
+            
+        </div>    
+    </div>
+
+
+
+   <!-- Optional JavaScript -->
+   <!-- JQuery first, then Popper.js, then bootstrap JS -->
+   <script src="http://code.jquery.com/jquery-3.3.1.slim.min.js"></script>
+   <script src="http://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3.umd/popper.min.js"></script>
+   <script src="http://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js"></script>
+ </body>
+</hmtl>
